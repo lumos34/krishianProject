bb6df45e37da155da56b6e0a226ac863bc1cd778
lumos34 <tajmiul.mec.eee.bd@gmail.com>
2021-12-23 15:51:07 +0600

    first commit

---
+++ lumos/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(lumos());
+}
+
+class lumos extends StatelessWidget {
+  const lumos({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("nfdfbhb"),
+          ),
+        ),
+      ),
+    );
+  }
+}
